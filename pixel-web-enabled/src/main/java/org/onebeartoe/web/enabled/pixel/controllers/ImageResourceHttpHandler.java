d2bc23d4ae889a0e5c301375dcaa82e8f3e2105b
Mr Kai <mrkai@snitchesgetstitch.es>
2020-06-30 16:16:52 -0700

    Added print like Kotlin version

--- pixel-web-enabled/src/main/java/org/onebeartoe/web/enabled/pixel/controllers/ImageResourceHttpHandler.java
+++ pixel-web-enabled/src/main/java/org/onebeartoe/web/enabled/pixel/controllers/ImageResourceHttpHandler.java
@@ -69,7 +69,7 @@ public abstract class ImageResourceHttpHandler extends TextHttpHandler
             URI requestURI = exchange.getRequestURI();
             
             String path = requestURI.getPath();
-            
+            System.out.println("Requested: " + requestURI.getPath());
             URL url = new URL("http://pixelcadedx.local:8080" + requestURI.getPath());
             HttpURLConnection con = (HttpURLConnection) url.openConnection();
             con.setRequestMethod("GET");
